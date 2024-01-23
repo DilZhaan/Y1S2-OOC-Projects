c2be1b6720b18ec55329797d1e2d0742f269d6aa
ChamithDilshan <61306553+DilZhaan@users.noreply.github.com>
2024-01-23 10:45:03 +0530

    GreetingsConsoleApp

---
+++ Labs/01/GreetingsConsoleApp/GreetingsConsoleApp.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+	cout << "Hello\n";
+	char ch;
+	cin >> ch; // Without an input command visual studio
+	// runs the code and automatically returns to the editor
+	return 0;
+}
