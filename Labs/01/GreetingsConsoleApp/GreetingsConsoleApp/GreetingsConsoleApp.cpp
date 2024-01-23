c8796ce557dcffbb3b5fa0a4fa191d4d4f276e39
DilZhan <61306553+DilZhaan@users.noreply.github.com>
2024-01-23 10:27:20 +0530

    Delete Labs directory

--- Labs/01/GreetingsConsoleApp/GreetingsConsoleApp/GreetingsConsoleApp.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main(void) {
-	cout << "Hello" << endl;
-	char ch;
-	cin >> ch; // Without an input command visual studio
-	// runs the code and automatically returns to the editor
-	return 0;
-}
